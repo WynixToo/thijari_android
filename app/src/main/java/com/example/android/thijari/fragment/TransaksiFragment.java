e7a6bf94aed389437745f9c2fb5f152fe5c8e5d9
Seion Lee <seion@webjunction.biz>
2016-12-29 15:10:38 +0800

    Fixing webview loading issue.

--- app/src/main/java/com/example/android/thijari/fragment/TransaksiFragment.java
+++ app/src/main/java/com/example/android/thijari/fragment/TransaksiFragment.java
@@ -1,50 +1,40 @@
 package com.example.android.thijari.fragment;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import com.example.android.thijari.R;
-import com.example.android.thijari.adapter.TestRecyclerViewAdapter;
-import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
-import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator;
-import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by florentchampigny on 24/04/15.
  */
 public class TransaksiFragment extends BaseFragment {
 
-    static final boolean GRID_LAYOUT = false;
-    private static final int ITEM_COUNT = 100;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private List<Object> mContentItems = new ArrayList<>();
+    WebView webView;
 
     public static TransaksiFragment newInstance() {
         return new TransaksiFragment();
     }
-
-    ObservableScrollView mScrollView;
-    WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,15 +44,30 @@ public class TransaksiFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trans, container, false);
+        setupProgressBar(view);
+        return view;
+    }
 
-
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         webView = (WebView) view.findViewById(R.id.wvTransaction);
 
-        webView.setWebViewClient(new WebViewClient(){
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+        webView.addJavascriptInterface(new javaScriptInterface(getActivity()), "HtmlViewer");
+
+        webView.setWebViewClient(new WebViewClient() {
             // you tell the webclient you want to catch when a url is about to load
             @SuppressWarnings("deprecation")
             @Override
-            public boolean shouldOverrideUrlLoading(WebView  view, String  url){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.d("WebView", url);
                 webView.loadUrl(url);
                 return true;
             }
@@ -71,14 +76,6 @@ public class TransaksiFragment extends BaseFragment {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 return shouldOverrideUrlLoading(view, request.getUrl().toString());
-            }
-
-            // here you execute an action when the URL you want is about to load
-            @Override
-            public void onLoadResource(WebView  view, String  url){
-                if( url.equals("http://cnn.com") ){
-                    // do whatever you want
-                }
             }
 
             @SuppressWarnings("deprecation")
@@ -92,6 +89,61 @@ public class TransaksiFragment extends BaseFragment {
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
                 onReceivedError(view, error.getErrorCode(), error.getDescription().toString(), request.getUrl().toString());
             }
+
+
+            @Override
+            public void onReceivedSslError(WebView view, final SslErrorHandler handler, SslError error) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                String message = "SSL Certificate error.";
+                switch (error.getPrimaryError()) {
+                    case SslError.SSL_UNTRUSTED:
+                        message = "The certificate authority is not trusted.";
+                        break;
+                    case SslError.SSL_EXPIRED:
+                        message = "The certificate has expired.";
+                        break;
+                    case SslError.SSL_IDMISMATCH:
+                        message = "The certificate Hostname mismatch.";
+                        break;
+                    case SslError.SSL_NOTYETVALID:
+                        message = "The certificate is not yet valid.";
+                        break;
+                }
+                message += " Do you want to continue anyway?";
+
+                builder.setTitle("SSL Certificate Error");
+                builder.setMessage(message);
+                builder.setPositiveButton("continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.proceed();
+                    }
+                });
+                builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        handler.cancel();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+
+            //This is all for debug purpose. Should comment out when compile release.
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                isProgressBarShown(false);
+                Log.d("WebView","Finish loading: '" + url );
+
+                webView.loadUrl("javascript:window.HtmlViewer.showHTML" +
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                isProgressBarShown(true);
+            }
         });
 
         webView.setWebChromeClient(new WebChromeClient() {
@@ -102,9 +154,26 @@ public class TransaksiFragment extends BaseFragment {
             }
         });
 
+        //Change your URL here.
+        //webView.loadUrl("http://10.10.10.181/public/login.html");
+        //webView.loadUrl("http://mnsolutions.ddns.net/projects/thijari/public/login.html");
         webView.loadUrl("http://yahoo.com");
+    }
 
-        return view;
+    class javaScriptInterface {
+
+        private Context ctx;
+
+        javaScriptInterface(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @JavascriptInterface
+        public void showHTML(String html) {
+            Log.d("JSInterface", html);
+            //new AlertDialog.Builder(ctx).setTitle("HTML").setMessage(html)
+            //        .setPositiveButton(android.R.string.ok, null).setCancelable(false).create().show();
+        }
 
     }
 
