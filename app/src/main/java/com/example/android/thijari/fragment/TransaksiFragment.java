2ba40d251f552ec24544a4ee5d39c004c8089b0b
Seion Lee <seion@webjunction.biz>
2016-12-29 10:24:49 +0800

    Added WebView into balance checking page.

--- app/src/main/java/com/example/android/thijari/fragment/TransaksiFragment.java
+++ app/src/main/java/com/example/android/thijari/fragment/TransaksiFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.thijari.fragment;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.example.android.thijari.R;
 import com.example.android.thijari.adapter.TestRecyclerViewAdapter;
@@ -35,11 +44,68 @@ public class TransaksiFragment extends BaseFragment {
     }
 
     ObservableScrollView mScrollView;
+    WebView webView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_trans, container, false);
+
+
+        webView = (WebView) view.findViewById(R.id.wvTransaction);
+
+        webView.setWebViewClient(new WebViewClient(){
+            // you tell the webclient you want to catch when a url is about to load
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView  view, String  url){
+                webView.loadUrl(url);
+                return true;
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return shouldOverrideUrlLoading(view, request.getUrl().toString());
+            }
+
+            // here you execute an action when the URL you want is about to load
+            @Override
+            public void onLoadResource(WebView  view, String  url){
+                if( url.equals("http://cnn.com") ){
+                    // do whatever you want
+                }
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getActivity(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+
+            @TargetApi(Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                onReceivedError(view, error.getErrorCode(), error.getDescription().toString(), request.getUrl().toString());
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                getActivity().setProgress(progress * 1000);
+            }
+        });
+
+        webView.loadUrl("http://yahoo.com");
+
         return view;
 
     }
+
 }
