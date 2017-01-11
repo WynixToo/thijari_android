7f0d40b44656b4b30d8cb72fe8c1835970a84cba
WynixToo <noob_THW@hotmail.com>
2017-01-11 19:49:17 +0800

    fix android 6 permission

--- app/src/main/java/com/example/android/thijari/fragment/MarkerGoogleMapFragment.java
+++ app/src/main/java/com/example/android/thijari/fragment/MarkerGoogleMapFragment.java
@@ -14,7 +14,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -81,14 +83,16 @@ public class MarkerGoogleMapFragment extends Fragment implements OnMapReadyCallb
     }
 
     private boolean checkLocationPermission() {
+        boolean result;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (getActivity().checkSelfPermission(Manifest.permission_group.LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission_group.LOCATION}, PERMISSIONS_REQUEST_LOCATION);
-                return false;
-            }
-            return true;
-        }
-        return true;
+            if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_REQUEST_LOCATION);
+                result = false;
+            } else
+                result = true;
+        } else
+            result = true;
+        return result;
     }
 
     @Override
